0e617618f0f34a2c4064f7fb30923b6035059e43
ksyasuda <ksyasuda@umich.edu>
2020-03-12 15:27:00 -0400

    Add color to output

--- headers/board.h
+++ headers/board.h
@@ -4,10 +4,23 @@
 #include <vector>
 #include <fstream>
 
+constexpr auto RED = "\033[31m";
+constexpr auto GREEN = "\033[32m";
+constexpr auto YELLOW = "\033[33m";
+constexpr auto BLUE = "\033[34m";
+constexpr auto WHITE = "\033[90m";
+constexpr auto ORANGE = "\033[96m";
+constexpr auto BLACK = "\033[93m";
+constexpr auto PURPLE = "\033[95m";
+constexpr auto CYAN = "\033[36m";
+constexpr auto RESET = "\033[0m";
+
 class Board {
 public:
 	std::vector<std::vector<int>> board;
 	std::vector<std::vector<int>> output_board;
+	const char* colors[11] = {RED, ORANGE, YELLOW, GREEN, BLUE, CYAN, PURPLE, BLACK,
+	WHITE, RESET };
 	int size;
 	int remaining_squares = 0;
 	int count = 0;
